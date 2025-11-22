a7e6e9c351828db2ab0962fd06e3df03fe3971a9
jangtjdgns <jangtjdgns0208@gmail.com>
2025-11-22 23:27:05 +0900

    jar → war 패키징 변경

--- src/main/resources/com/JSHblog/jangtjdgns/JangtjdgnsApplication.java
+++ src/main/resources/com/JSHblog/jangtjdgns/JangtjdgnsApplication.java
@@ -2,12 +2,19 @@ package com.JSHblog.jangtjdgns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JangtjdgnsApplication {
+public class JangtjdgnsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JangtjdgnsApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // War 배포 시 실행될 클래스 지정
+        return builder.sources(JangtjdgnsApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(JangtjdgnsApplication.class, args);
+    }
 }
